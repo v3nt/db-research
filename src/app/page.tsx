52bfd08da4dcbc430da60820cfcd5abd5896a055
v3nt <dan@jynk.net>
2024-09-29 12:36:17 +0100

    get coutries api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,42 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState([]);
+  const base = process.env.NEXT_PUBLIC_COUNTRIES_BASE_URL;
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const all = `${base}/all`;
+        const reducedDataPoints = `${base}/all?fields=name,flags,population,cca2,country,currency,capital`;
+        const response = await fetch(all);
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching countries data:", error);
+      }
+    };
+
+    fetchCountries();
+
+    const fetchCountry = async (id) => {
+      try {
+        const all = `${base}/name/${id}`;
+        const reducedDataPoints = `${base}/all?fields=name,flags,population,cca2,country,currency,capital`;
+        const response = await fetch(all);
+        const data = await response.json();
+        setCountry(data);
+      } catch (error) {
+        console.error("Error fetching country data:", error);
+      }
+    };
+
+    fetchCountry("eesti");
+  }, []);
   return (
     <>
       <Image
@@ -11,6 +47,17 @@ export default function Home() {
         height={38}
         priority
       />
+      <ul>
+        {countries.map((country) => (
+          <li key={country.cca2}>
+            <h1 className="text-lg font-bold">{country.name.common}</h1>
+            <ul>
+              <li>{country.cca2}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+
       <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
         <li className="mb-2">
           Get started by editing{" "}
@@ -23,21 +70,6 @@ export default function Home() {
       </ol>
 
       <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            className="dark:invert"
-            src="https://nextjs.org/icons/vercel.svg"
-            alt="Vercel logomark"
-            width={20}
-            height={20}
-          />
-          Deploy now
-        </a>
         <a
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
